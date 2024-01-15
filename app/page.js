eb62f8bb8de6e0d6662d6e7ae359220a842ff345
Ashkan Khani <drashkankhani@gmail.com>
2024-01-15 19:55:48 +0330

    add my name :)

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Game from './components/Game'
 
 export default function Home() {
@@ -6,6 +5,13 @@ export default function Home() {
     <main className="md:w-screen md:h-screen bg-slate-800">
       <h1 className='text-center text-white text-5xl pt-10'>بازی سنگ کاغذ قیچی</h1>
       <Game />
+      
+      <h2 className='mt-72 pb-2 text-3xl text-center text-slate-300'>
+        developed by <a 
+          className='font-bold'
+          href='https://github.com/ashkankhani/'>Ashkan Khani</a>
+      </h2>
+
     </main>
   )
 }
