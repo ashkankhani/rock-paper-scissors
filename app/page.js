bf3203691a49b19d545ecc9da4df45b9cb1b7a04
Ashkan Khani <drashkankhani@gmail.com>
2024-01-15 20:00:27 +0330

    a little change

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className='text-center text-white text-5xl pt-10'>بازی سنگ کاغذ قیچی</h1>
       <Game />
       
-      <h2 className='mt-72 pb-2 text-3xl text-center text-slate-300'>
+      <h2 className='mt-60 pb-2 text-3xl text-center text-slate-300'>
         developed by <a 
           className='font-bold'
           href='https://github.com/ashkankhani/'>Ashkan Khani</a>
